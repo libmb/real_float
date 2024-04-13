db23613181b89c94b6e787e47a4c287782443ed4
Juyeong Maing <mjy9088@naver.com>
2024-04-14 02:46:14 +0900

    add Windows support

--- build/misc/compile_commands.json.init.sh
+++ build/misc/compile_commands.json.init.sh
@@ -6,5 +6,11 @@ cd "$(dirname "$0")/../.."
 
 trap 'rm -f compile_commands.json.tmp' EXIT;
 
-sed "s:\[\[WORKSPACE\]\]:$(pwd):g" build/misc/compile_commands.json.template > compile_commands.json.tmp
+if command -v cmd > /dev/null; then
+  PWD="$(cmd //c cd | sed 's|\\|\\\\\\\\|g' | sed s/\"/\\\"/g)"
+else
+  PWD="$(pwd | sed 's|\\|\\\\\\\\|g' | sed s/\"/\\\"/g)"
+fi
+
+sed "s\\[\\[WORKSPACE\\]\\]$PWDg" build/misc/compile_commands.json.template > compile_commands.json.tmp
 mv compile_commands.json.tmp compile_commands.json
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+
+sh build/misc/compile_commands.json.init.sh
